1bd9d2f4af32c5f599f1316d9f08a6d5d1ce52f0
Nahid Hasan Sojib <247019775+nahid2221@users.noreply.github.com>
2026-08-18 15:47:46 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+let cartCount = 0;
+
+function addToCart() {
+
+    cartCount++;
+
+    document.getElementById("cartCount").innerText =
+        cartCount;
+
+    alert("Product added to cart 🛒");
+
+}
